fdc33cbef8fad52193076dc2278ac5a212a277ea
Juergen Hock <juergen.hock@gmail.com>
2019-07-16 20:37:36 +0200

    cleanup

--- src/assets/js/toastr/toastr.js
+++ src/assets/js/toastr/toastr.js
@@ -9,17 +9,3 @@ require.config(
     'toastr': 'assets/js/toastr/js/toastr-2.1.3.min',
   }
 });
-
-/*
-require(['toastr', 'jquery'], function()
-{
-  $(function()
-  {
-    toastr.options =
-    {
-      positionClass: 'toast-top-right',
-      timeOut: 3000
-    };
-  });
-});
-*/
